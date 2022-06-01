194e0e1699c215172aab444d9627edae0763a0bb
Sinkevych Roman <roman@auctionsoftware.com>
2022-06-01 23:18:45 +0300

    fix bug with background crash

--- android/app/src/main/java/com/tradebid/MainActivity.java
+++ android/app/src/main/java/com/tradebid/MainActivity.java
@@ -3,17 +3,30 @@ package com.tradebid;
 import com.facebook.react.ReactActivity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule
      * rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
         return "TradeBid";
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.clear();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
     }
 
     @Override
